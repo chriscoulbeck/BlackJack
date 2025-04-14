4aaec8c62dd982a0ed13023d64b85181794bc2eb
chris <cdcoulbeck@gmail.com>
2025-04-14 17:36:29 +1200

    Create Deck class with card generation and shuffling

---
+++ src/main/java/model/Deck.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+/**
+ * Represents a deck of playing cards.
+ * <p>
+ * This class provides functionality to create a standard 52-card deck, shuffle the deck, and retrieve the deck of cards.
+ * The deck is initially generated in a standard order (Ace to King of each suit) and can be shuffled for use in the game.
+ */
+public class Deck {
+    /**
+     * The stack of cards in this deck.
+     * Each card is represented by a {@link Card} object.
+     */
+    Stack<Card> cards;
+
+    /**
+     * Constructs a new deck of cards and generates a standard 52-card deck.
+     * The deck is created in the standard order and can be shuffled later.
+     */
+    public Deck() {
+        this.cards = generateStandardDeck();
+    }
+
+    /**
+     * Generates a standard deck of 52 playing cards.
+     * <p>
+     * This method creates a deck of cards by combining all suits (Hearts, Diamonds, Clubs, Spades)
+     * with all ranks (Ace, 2, 3, ..., King). The cards are added to a stack in the default order.
+     *
+     * @return a {@link Stack} containing the 52 playing cards in the standard order.
+     */
+    private Stack<Card> generateStandardDeck() {
+        List<String> suits = List.of("Hearts", "Diamonds", "Clubs", "Spades");
+        List<String> ranks = List.of("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King");
+
+        return suits.stream()
+                .flatMap(suit -> ranks.stream()
+                        .map(rank -> new Card(suit, rank)))
+                .collect(Collectors.toCollection(Stack::new));
+    }
+
+    /**
+     * Shuffles the deck of cards.
+     * <p>
+     * This method randomly shuffles the cards in the deck using {@link Collections#shuffle(java.util.List)}.
+     * It modifies the order of the cards in the deck, ensuring the cards are in a random order.
+     */
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    /**
+     * Retrieves the stack of cards in the deck.
+     *
+     * @return a {@link Stack} containing the cards in the deck.
+     */
+    public Stack<Card> getCards() {
+        return cards;
+    }
+}
