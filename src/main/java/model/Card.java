8aba68c84227d6d1486c85a6620c97511470ecf1
chris <cdcoulbeck@gmail.com>
2025-04-15 22:24:26 +1200

    Override toString method of the Card record

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -4,4 +4,8 @@ package model;
  * The {@code Card} record represents a playing card. All cards have a suit and rank.
  */
 public record Card(String suit, String rank) {
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
